835b4a03ff46651f4fb3d852cfa748965e6e0b0c
Ronald Rink <ronald.rink@d-fens.net>
2025-05-29 18:19:51 +0200

    minor cleanup

--- tests/test_MultiLineTextParser.py
+++ tests/test_MultiLineTextParser.py
@@ -20,8 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from dataclasses import asdict
-import json
 from typing import Callable
 import unittest
 
