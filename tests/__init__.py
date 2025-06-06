fabb6b6322fcb5b1fd6df0e48ffd7162da4194eb
Ronald Rink <ronald.rink@d-fens.net>
2025-06-06 12:29:31 +0200

    minor: formatting and doc

--- tests/__init__.py
+++ tests/__init__.py
@@ -23,4 +23,9 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+
+# run from project root with either
+# $ python -m unittest discover
+# or
+# $ python -m unittest discover tests .
