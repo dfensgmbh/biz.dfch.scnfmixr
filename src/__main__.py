55835428d77528b40ed960a381ba11a4a5a5de8f
Ronald Rink <ronald.rink@d-fens.net>
2025-06-10 05:58:01 +0200

    __main__
    * refactored code from __main__here
    * removed pylint warnings
    * made channel and rates selection dynamic
    * currently fixd to working only with J75 (c1, 32kHz) and c1/48kHz devices

--- src/__main__.py
+++ src/__main__.py
@@ -20,80 +20,149 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+"""The entry module to the programme."""
+
 import argparse
-import subprocess
 import sys
 import time
 
-from env_embedded import Asound, Usb
+from alsa_usb import AlsaStreamInfoParser
 from jack_commands import JackConnection, ZitaBridgeAlsaToJack, ZitaBridgeJackToAlsa
 from log import log
+from service import SetupDevice
 from Version import Version
 
 
 def run_loop():
-    try:
-        # Detect Jabra SPEAK 510
-        device_info_lcl = Usb.get_usb_device_info("1-1")
-        asound_info_lcl = Asound.get_info(device_info_lcl)
-        wav_file = "/home/admin/PhoneTap20/src/snd/CardA.Connected.EN.wav"
-        params = ["aplay", "-D", f"plughw:{asound_info_lcl.idCard}", wav_file]
-        _ = subprocess.run(params)
+    """Main run loop of programme."""
 
+    # Detect local speaker phone
+    usb_id = "1-1"
+    log.info("LCL: Detecting local speakerphone [%s] ...", usb_id)
+    device_lcl = SetupDevice.Factory.create(usb_id)
+
+    # Detect external phone EX1
+    usb_id = "1-2"
+    log.info("EX1: Detecting external device 1 [%s] ...", usb_id)
+    device_ex1 = SetupDevice.Factory.create(usb_id)
+
+    # Detect external recorder
+    usb_id = "3-2"
+    log.info("REC: Detecting recorder [%s] ...", usb_id)
+    device_rec = SetupDevice.Factory.create(usb_id)
+
+    while True:
         try:
-            # Detect Sound Devices MixPre-3 II
-            device_info_rec = Usb.get_usb_device_info("3-1")
-            asound_info_ex1 = Asound.get_info(device_info_rec)
-            wav_file = "/home/admin/PhoneTap20/src/snd/CardA.Connected.DE.wav"
-            params = ["aplay", "-D", f"plughw:{asound_info_ex1.idCard}", wav_file]
-            _ = subprocess.run(params)
 
-            try:
-                # JABRA
-                _ = ZitaBridgeAlsaToJack("LCL-I", f"hw:{asound_info_lcl.idCard},0", 1, 16000)
-                _ = ZitaBridgeJackToAlsa("LCL-O", f"hw:{asound_info_lcl.idCard},0", 2, 48000)
+            # LCL
+            log.info("LCL: Creating JACK clients for '%s' ...", device_lcl.asound_info.idCard)
 
-                # MixPre-3
-                _ = ZitaBridgeAlsaToJack("EX1-I", f"hw:{asound_info_ex1.idCard},0", 2, 48000)
-                _ = ZitaBridgeJackToAlsa("EX1-O", f"hw:{asound_info_ex1.idCard},0", 2, 48000)
+            parser_lcl = AlsaStreamInfoParser(device_lcl.asound_info.idCard)
 
-                time.sleep(10)
+            lcl_capture = parser_lcl.get_best_capture_interface()
+            log.info("%s: %s", device_lcl.asound_info.idCard, lcl_capture.to_dict())
 
-                _ = JackConnection.Factory.create("LCL-I:capture_1", "EX1-O:playback_1")
-                _ = JackConnection.Factory.create("LCL-I:capture_1", "EX1-O:playback_2")
+            lcl_playback = parser_lcl.get_best_playback_interface()
+            log.info("%s: %s", device_lcl.asound_info.idCard, lcl_playback.to_dict())
 
-                _ = JackConnection.Factory.create("EX1-I:capture_1", "LCL-O:playback_1")
-                _ = JackConnection.Factory.create("EX1-I:capture_2", "LCL-O:playback_2")
+            _ = ZitaBridgeAlsaToJack(
+                "LCL-I",
+                f"hw:{device_lcl.asound_info.idCard},0",
+                lcl_capture.channel_count,
+                parser_lcl.best_rate(lcl_capture.rates),
+            )
+            _ = ZitaBridgeJackToAlsa(
+                "LCL-O",
+                f"hw:{device_lcl.asound_info.idCard},0",
+                lcl_playback.channel_count,
+                parser_lcl.best_rate(lcl_playback.rates),
+            )
 
-                time.sleep(60)
+            # EX1
+            log.info("EX1: Creating JACK clients for '%s' ...", device_ex1.asound_info.idCard)
 
-            except Exception:
-                time.sleep(1)
+            parser_ex1 = AlsaStreamInfoParser(device_ex1.asound_info.idCard)
 
-        except Exception:
+            ex1_capture = parser_ex1.get_best_capture_interface()
+            log.info("%s: %s", device_ex1.asound_info.idCard, ex1_capture.to_dict())
+
+            ex1_playback = parser_ex1.get_best_playback_interface()
+            log.info("%s: %s", device_ex1.asound_info.idCard, ex1_playback.to_dict())
+
+            _ = ZitaBridgeAlsaToJack(
+                "EX1-I",
+                f"hw:{device_ex1.asound_info.idCard},0",
+                ex1_capture.channel_count,
+                parser_ex1.best_rate(ex1_capture.rates),
+            )
+            _ = ZitaBridgeJackToAlsa(
+                "EX1-O",
+                f"hw:{device_ex1.asound_info.idCard},0",
+                ex1_playback.channel_count,
+                parser_ex1.best_rate(ex1_playback.rates),
+            )
+
+            # REC
+            log.info("REC: Creating JACK clients for '%s' ...", device_rec.asound_info.idCard)
+
+            parser_rec = AlsaStreamInfoParser(device_rec.asound_info.idCard)
+
+            rec_capture = parser_rec.get_best_capture_interface()
+            log.info("%s: %s", device_rec.asound_info.idCard, rec_capture.to_dict())
+
+            rec_playback = parser_rec.get_best_playback_interface()
+            log.info("%s: %s", device_rec.asound_info.idCard, rec_playback.to_dict())
+
+            _ = ZitaBridgeAlsaToJack(
+                "REC-I",
+                f"hw:{device_rec.asound_info.idCard},0",
+                rec_capture.channel_count,
+                parser_rec.best_rate(rec_capture.rates),
+            )
+            _ = ZitaBridgeJackToAlsa(
+                "REC-O",
+                f"hw:{device_rec.asound_info.idCard},0",
+                rec_playback.channel_count,
+                parser_rec.best_rate(rec_playback.rates),
+            )
+
+            # DFTODO - fix timing and then remove this
+            time.sleep(3)
+
+            log.info("Creating JACK connections for '%s' ...", device_lcl.asound_info.idCard)
+            _ = JackConnection.Factory.create("LCL-I:capture_1", "EX1-O:playback_1")
+            _ = JackConnection.Factory.create("LCL-I:capture_1", "EX1-O:playback_2")
+            _ = JackConnection.Factory.create("LCL-I:capture_1", "REC-O:playback_1")
+            _ = JackConnection.Factory.create("LCL-I:capture_1", "REC-O:playback_2")
+
+            log.info("Creating JACK connections for '%s' ...", device_ex1.asound_info.idCard)
+            _ = JackConnection.Factory.create("EX1-I:capture_1", "LCL-O:playback_1")
+            _ = JackConnection.Factory.create("EX1-I:capture_1", "LCL-O:playback_2")
+            _ = JackConnection.Factory.create("EX1-I:capture_1", "REC-O:playback_1")
+            _ = JackConnection.Factory.create("EX1-I:capture_1", "REC-O:playback_2")
+
+            while True:
+                log.debug("Keep alive ...")
+                time.sleep(5)
+
+        except Exception:  # pylint: disable=broad-exception-caught
             time.sleep(1)
-
-        time.sleep(5)
-
-    except Exception:
-        # wav_file = '/home/admin/PhoneTap20/src/snd/CardA.Disconnected.EN.wav'
-        # params = ['aplay', '-D', f'plughw:{asound_info.idCard}', wav_file]
-        # result = subprocess.run(params)
-        time.sleep(1)
 
 
 def arg_startup():
+    """This function handles argument 'startup'."""
     while True:
         try:
-            log.info(f"Running script as startup ... '{time.localtime().tm_sec}'")
+            log.info("Running script as startup ... '%s'", time.localtime().tm_sec)
             run_loop()
             time.sleep(1)
-        except Exception as e:
-            log.info(f"startup() FAILED with exception '{e}'. Restarting ...")
+        except Exception as ex:  # pylint: disable=broad-exception-caught
+            log.info("startup() FAILED with exception '%s'. Restarting ...", ex)
             time.sleep(5)
 
 
 def main():
+    """The entry point to the programme."""
     Version().ensure_minimum_version(3, 10)
 
     log.info("Application started. Parsing command-line arguments ...")
