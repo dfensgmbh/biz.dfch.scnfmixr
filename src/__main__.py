8f3133d68de3ee0d421731890140d9871bf3cc2e
Ronald Rink <ronald.rink@d-fens.net>
2025-07-03 22:38:07 +0200

    minor formatting

--- src/__main__.py
+++ src/__main__.py
@@ -27,7 +27,9 @@ import sys
 import time
 
 from alsa_usb import AlsaStreamInfoParser
-from jack_commands import JackConnection, ZitaBridgeAlsaToJack, ZitaBridgeJackToAlsa
+from jack_commands import JackConnection
+from jack_commands import ZitaBridgeAlsaToJack
+from jack_commands import ZitaBridgeJackToAlsa
 from log import log
 from service import SetupDevice
 from Version import Version
@@ -55,15 +57,18 @@ def run_loop():
         try:
 
             # LCL
-            log.info("LCL: Creating JACK clients for '%s' ...", device_lcl.asound_info.idCard)
+            log.info("LCL: Creating JACK clients for '%s' ...",
+                     device_lcl.asound_info.idCard)
 
             parser_lcl = AlsaStreamInfoParser(device_lcl.asound_info.idCard)
 
             lcl_capture = parser_lcl.get_best_capture_interface()
-            log.info("%s: %s", device_lcl.asound_info.idCard, lcl_capture.to_dict())
+            log.info("%s: %s",
+                     device_lcl.asound_info.idCard, lcl_capture.to_dict())
 
             lcl_playback = parser_lcl.get_best_playback_interface()
-            log.info("%s: %s", device_lcl.asound_info.idCard, lcl_playback.to_dict())
+            log.info("%s: %s",
+                     device_lcl.asound_info.idCard, lcl_playback.to_dict())
 
             _ = ZitaBridgeAlsaToJack(
                 "LCL-I",
@@ -79,15 +84,18 @@ def run_loop():
             )
 
             # EX1
-            log.info("EX1: Creating JACK clients for '%s' ...", device_ex1.asound_info.idCard)
+            log.info("EX1: Creating JACK clients for '%s' ...",
+                     device_ex1.asound_info.idCard)
 
             parser_ex1 = AlsaStreamInfoParser(device_ex1.asound_info.idCard)
 
             ex1_capture = parser_ex1.get_best_capture_interface()
-            log.info("%s: %s", device_ex1.asound_info.idCard, ex1_capture.to_dict())
+            log.info("%s: %s",
+                     device_ex1.asound_info.idCard, ex1_capture.to_dict())
 
             ex1_playback = parser_ex1.get_best_playback_interface()
-            log.info("%s: %s", device_ex1.asound_info.idCard, ex1_playback.to_dict())
+            log.info("%s: %s",
+                     device_ex1.asound_info.idCard, ex1_playback.to_dict())
 
             _ = ZitaBridgeAlsaToJack(
                 "EX1-I",
@@ -103,15 +111,18 @@ def run_loop():
             )
 
             # REC
-            log.info("REC: Creating JACK clients for '%s' ...", device_rec.asound_info.idCard)
+            log.info("REC: Creating JACK clients for '%s' ...",
+                     device_rec.asound_info.idCard)
 
             parser_rec = AlsaStreamInfoParser(device_rec.asound_info.idCard)
 
             rec_capture = parser_rec.get_best_capture_interface()
-            log.info("%s: %s", device_rec.asound_info.idCard, rec_capture.to_dict())
+            log.info("%s: %s", device_rec.asound_info.idCard,
+                     rec_capture.to_dict())
 
             rec_playback = parser_rec.get_best_playback_interface()
-            log.info("%s: %s", device_rec.asound_info.idCard, rec_playback.to_dict())
+            log.info("%s: %s", device_rec.asound_info.idCard,
+                     rec_playback.to_dict())
 
             _ = ZitaBridgeAlsaToJack(
                 "REC-I",
@@ -129,17 +140,27 @@ def run_loop():
             # DFTODO - fix timing and then remove this
             time.sleep(3)
 
-            log.info("Creating JACK connections for '%s' ...", device_lcl.asound_info.idCard)
-            _ = JackConnection.Factory.create("LCL-I:capture_1", "EX1-O:playback_1")
-            _ = JackConnection.Factory.create("LCL-I:capture_1", "EX1-O:playback_2")
-            _ = JackConnection.Factory.create("LCL-I:capture_1", "REC-O:playback_1")
-            _ = JackConnection.Factory.create("LCL-I:capture_1", "REC-O:playback_2")
+            log.info("Creating JACK connections for '%s' ...",
+                     device_lcl.asound_info.idCard)
+            _ = JackConnection.Factory.create("LCL-I:capture_1",
+                                              "EX1-O:playback_1")
+            _ = JackConnection.Factory.create("LCL-I:capture_1",
+                                              "EX1-O:playback_2")
+            _ = JackConnection.Factory.create("LCL-I:capture_1",
+                                              "REC-O:playback_1")
+            _ = JackConnection.Factory.create("LCL-I:capture_1",
+                                              "REC-O:playback_2")
 
-            log.info("Creating JACK connections for '%s' ...", device_ex1.asound_info.idCard)
-            _ = JackConnection.Factory.create("EX1-I:capture_1", "LCL-O:playback_1")
-            _ = JackConnection.Factory.create("EX1-I:capture_1", "LCL-O:playback_2")
-            _ = JackConnection.Factory.create("EX1-I:capture_1", "REC-O:playback_1")
-            _ = JackConnection.Factory.create("EX1-I:capture_1", "REC-O:playback_2")
+            log.info("Creating JACK connections for '%s' ...",
+                     device_ex1.asound_info.idCard)
+            _ = JackConnection.Factory.create("EX1-I:capture_1",
+                                              "LCL-O:playback_1")
+            _ = JackConnection.Factory.create("EX1-I:capture_1",
+                                              "LCL-O:playback_2")
+            _ = JackConnection.Factory.create("EX1-I:capture_1",
+                                              "REC-O:playback_1")
+            _ = JackConnection.Factory.create("EX1-I:capture_1",
+                                              "REC-O:playback_2")
 
             while True:
                 log.debug("Keep alive ...")
@@ -153,7 +174,8 @@ def arg_startup():
     """This function handles argument 'startup'."""
     while True:
         try:
-            log.info("Running script as startup ... '%s'", time.localtime().tm_sec)
+            log.info("Running script as startup ... '%s'",
+                     time.localtime().tm_sec)
             run_loop()
             time.sleep(1)
         except Exception as ex:  # pylint: disable=broad-exception-caught
@@ -163,11 +185,15 @@ def arg_startup():
 
 def main():
     """The entry point to the programme."""
+
     Version().ensure_minimum_version(3, 10)
 
     log.info("Application started. Parsing command-line arguments ...")
+
     parser = argparse.ArgumentParser(description="PhoneTap20 Main Script")
-    parser.add_argument("--startup", "-s", action="store_true", help="PhoneTap20 startup loop")
+    parser.add_argument("--startup", "-s",
+                        action="store_true",
+                        help="PhoneTap20 startup loop")
     args = parser.parse_args()
 
     if args.startup:
