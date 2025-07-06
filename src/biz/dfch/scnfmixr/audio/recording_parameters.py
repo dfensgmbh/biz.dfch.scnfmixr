62b9ba39329511407179dcdfdb78e0aea1fababf
Ronald Rink <ronald.rink@d-fens.net>
2025-07-06 21:17:19 +0200

    added package audio

---
+++ src/biz/dfch/scnfmixr/audio/recording_parameters.py
@@ -0,0 +1,43 @@
+# MIT License
+
+# Copyright (c) 2025 d-fens GmbH, http://d-fens.ch
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Recording parameters set by the user."""
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class RecordingParameters():
+    """Recording parameters.
+
+    Attributes:
+        format (str): The format and codec of the recording.
+        sampling_rate (int): The sampling rate of the recording.
+        bit_depth (int): Bits per sample of the recording.
+        is_dual (bool): True, if recording will be written to 2 storage
+            devices; false otherwise (default). 
+    """
+
+    format: str
+    sampling_rate: int
+    bit_depth: int
+    is_dual: bool
