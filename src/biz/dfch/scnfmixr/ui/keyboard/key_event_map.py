7a94575522404311dae6d7af1bb77f4667e73a62
Ronald Rink <ronald.rink@d-fens.net>
2025-07-10 06:35:53 +0200

    adjusted keyboard mapping
    * include TAB, BACKSPACE and ENTER
    * added normal dot (in addition to kp_dot)

--- src/biz/dfch/scnfmixr/ui/keyboard/key_event_map.py
+++ src/biz/dfch/scnfmixr/ui/keyboard/key_event_map.py
@@ -55,6 +55,7 @@ class KeyEventMap(StrEnum):
     KEY_8 = "8"
     KEY_9 = "9"
 
+    KEY_DOT = "."
     KEY_KPDOT = "."
 
     KEY_KPPLUS = "+"
@@ -63,10 +64,10 @@ class KeyEventMap(StrEnum):
     KEY_KPSLASH = "/"
     KEY_EQUAL = "="
 
-    KEY_KPENTER = "|"
-    KEY_ENTER = "|"
+    KEY_KPENTER = "Ö"
+    KEY_ENTER = "Ö"
 
-    KEY_TAB = "?"
-    KEY_BACKSPACE = "?"
+    KEY_TAB = "Ü"
+    KEY_BACKSPACE = "Ä"
 
     KEY_NUMLOCK = "?"
