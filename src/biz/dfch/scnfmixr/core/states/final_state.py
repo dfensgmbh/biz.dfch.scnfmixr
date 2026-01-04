750a2b8fd9893e8f7ec62acc7143889bd221b51d
Ronald Rink <ronald.rink@d-fens.net>
2026-01-04 17:56:22 +0100

    FinalState
    * add shutdown command

--- src/biz/dfch/scnfmixr/core/states/final_state.py
+++ src/biz/dfch/scnfmixr/core/states/final_state.py
@@ -17,7 +17,10 @@
 
 from __future__ import annotations
 from enum import StrEnum
+import time
 
+from biz.dfch.asyn import Process
+from biz.dfch.asyn import ThreadPool
 from biz.dfch.logging import log
 
 from ...app import ApplicationContext
@@ -31,6 +34,11 @@ from ...public.system.messages import SystemMessage
 
 class FinalState(StateBase):
     """Implements FinalState of the application."""
+
+    _SUDO_FULLNAME = "/usr/bin/sudo"
+    _SHUTDOWN_FULLNAME = "/usr/sbin/shutdown"
+    _SHUTDOWN_OPT_HALT = "-h"
+    _SHUTDOWN_OPT_NOW = "now"
 
     class Event(StrEnum):
         """Events for this state."""
@@ -71,9 +79,30 @@ class FinalState(StateBase):
 
         log.info("Application context: '%s'.", app_ctx)
 
+        tp = ThreadPool.Factory.get(self.__class__.__name__, 1)
+        tp.invoke(self._shutdown)
+
         log.info("Initiating application termination COMPLETED.")
 
         return True
+
+    def _shutdown(self, wait_time_ms: int = 10000) -> None:
+        """Stop the system."""
+
+        log.debug("Try to stop the system. Waiting '%s' ms ...", wait_time_ms)
+
+        time.sleep(wait_time_ms / 1000)
+
+        log.debug("Try to stop the system now.")
+        cmd: list[str] = [
+            self._SUDO_FULLNAME,
+            self._SHUTDOWN_FULLNAME,
+            self._SHUTDOWN_OPT_HALT,
+            self._SHUTDOWN_OPT_NOW,
+        ]
+        log.info("Try to stop the system now. Pending ...")
+
+        Process.start(cmd, wait_on_completion=True)
 
     def on_leave(self, ctx: ExecutionContext) -> None:
         """Invoked upon leaving the state.
