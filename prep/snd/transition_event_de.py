9e5e14a3a1948623b139079ca76b072099520d2f
Irene Hofstetter <irene.hofstetter@npower.ch>
2026-02-15 20:03:44 +0100

    Transition Events DE wav *added wav for transition events DE

--- prep/snd/transition_event_de.py
+++ prep/snd/transition_event_de.py
@@ -54,128 +54,146 @@ Eingabegerät ZWEI übersprungen.
 """,
 
     # Menu: Detect HID HI3.
+    # OK
 
     TransitionEvent.DETECTING_DEVICE_HI3_ENTER: """
 Versuche, Eingabegerät DREI zu erkennen.
 """,
 
     # DFTODO: Duplicate intro sound from HI1.
-    TransitionEvent.DETECTING_DEVICE_HI3_LEAVE: """""",
+    # OK
+    TransitionEvent.DETECTING_DEVICE_HI3_LEAVE: """
+<<<sound-intro.wav>>>""",
 
     TransitionEvent.SKIPPING_DEVICE_HI3_LEAVE: """
 Eingabegerät DREI übersprungen.
 """,
 
     # Menu: Detect Audio LCL.
+    # OK. No audio menu necessary.
 
-    TransitionEvent.DETECTING_DEVICE_LCL_ENTER: """
-guid:902be5b8-cde4-4e89-b2d1-a531b70bed4e
-""",
+    TransitionEvent.DETECTING_DEVICE_LCL_ENTER: """""",
 
-    TransitionEvent.DETECTING_DEVICE_LCL_LEAVE: """
-guid:a2ff549b-b12c-4fe7-8f46-0500f3689327
-""",
+    TransitionEvent.DETECTING_DEVICE_LCL_LEAVE: """""",
 
-    TransitionEvent.SKIPPING_DEVICE_LCL_LEAVE: """
-guid:58411c9d-5876-4242-9862-9e10e3ad4846
-""",
+    TransitionEvent.SKIPPING_DEVICE_LCL_LEAVE: """""",
 
     # Menu: Detect Audio EX1.
+    # OK
 
     TransitionEvent.DETECTING_DEVICE_EX1_ENTER: """
 Versuche, externes Gerät EX1 zu erkennen.
 """,
 
+    # OK
     TransitionEvent.DETECTING_DEVICE_EX1_LEAVE: """
 Externes Gerät EX1 erfolgreich erkannt.
 """,
 
+    # OK
     TransitionEvent.DETECTING_DEVICE_EX1_FAILED: """
 Erkennung des externen Geräts EX1 fehlgeschlagen.
 
 Überprüfen Sie die Kabel und den Anschluss.
 """,
 
+    # OK
     TransitionEvent.SKIPPING_DEVICE_EX1_LEAVE: """
 Externes Gerät EX1 wurde übersprungen.
 """,
 
     # Menu: Detect Audio EX2.
 
+    # OK
     TransitionEvent.DETECTING_DEVICE_EX2_ENTER: """
 Versuche, externes Gerät EX2 zu erkennen.
 """,
 
+    # OK
     TransitionEvent.DETECTING_DEVICE_EX2_LEAVE: """
 Externes Gerät EX2 erfolgreich erkannt.
 """,
 
+    # OK
     TransitionEvent.DETECTING_DEVICE_EX2_FAILED: """
 Erkennung des externen Geräts EX2 fehlgeschlagen.
 
 Überprüfen Sie die Kabel und den Anschluss.
 """,
 
+    # OK
     TransitionEvent.SKIPPING_DEVICE_EX2_LEAVE: """
 Externes Gerät EX2 wurde übersprungen.
 """,
 
     # Menu: Detect storage RC1.
 
+    # OK
     TransitionEvent.DETECTING_DEVICE_RC1_ENTER: """
 Versuche, Speichergerät RC1 zu erkennen.
 """,
 
+    # OK
     TransitionEvent.DETECTING_DEVICE_RC1_LEAVE: """
 Speichergerät RC1 erfolgreich erkannt.
 """,
 
+    # OK
     TransitionEvent.DETECTING_DEVICE_RC1_FAILED: """
-Erkennung des Speichergerät RC1 fehlgeschlagen.
+Erkennung des Speichergeräts RC1 fehlgeschlagen.
 
 Überprüfen Sie die Kabel und den Anschluss.
 """,
 
+    # OK
     TransitionEvent.SKIPPING_DEVICE_RC1_LEAVE: """
 Speichergerät RC1 wurde übersprungen.
 """,
 
     # Menu: Detect storage RC2.
 
+    # OK
     TransitionEvent.DETECTING_DEVICE_RC2_ENTER: """
 Versuche, Speichergerät RC2 zu erkennen.
 """,
 
+    # OK
     TransitionEvent.DETECTING_DEVICE_RC2_LEAVE: """
 Speichergerät RC2 erfolgreich erkannt.
 """,
 
+    # OK
     TransitionEvent.DETECTING_DEVICE_RC2_FAILED: """
 Erkennung des Speichergerät RC2 fehlgeschlagen.
 
 Überprüfen Sie die Kabel und den Anschluss.
 """,
 
+    # OK
     TransitionEvent.SKIPPING_DEVICE_RC2_LEAVE: """
 Speichergerät RC2 wurde übersprungen.
 """,
 
     # Menu: Clean storage RC1.
 
+    # OK
     TransitionEvent.CLEANING_DEVICE_RC1_ENTER: """
 Versuche, Aufnahmen vom Speichergerät RC1 zu löschen.
 """,
 
+    # OK
     TransitionEvent.CLEANING_DEVICE_RC1_LEAVE: """
 Löschen der Aufnahmen vom Speichergerät RC1 erfolgreich.
 """,
 
     # Menu: Clean storage RC2.
 
+    # OK
     TransitionEvent.CLEANING_DEVICE_RC2_ENTER: """
 Versuche, Aufnahmen vom Speichergerät RC2 zu löschen.
 """,
 
+    # OK
     TransitionEvent.CLEANING_DEVICE_RC2_LEAVE: """
 Löschen der Aufnahmen vom Speichergerät RC2 erfolgreich.
 """,
@@ -229,65 +247,79 @@ Effektgerät 2 wurde übersprungen.
 
     # Menu: Initialise audio.
 
+    # OK
     TransitionEvent.INITIALISING_AUDIO_ENTER: """
 Audio wird initialisiert
 """,
 
+    # OK
     TransitionEvent.INITIALISING_AUDIO_LEAVE: """
 Audio ist initialisiert
 """,
 
     # Menu: Main.
 
+    # OK
     TransitionEvent.STARTING_RECORDING_ENTER: """
-Bereite Aufnahme vor.
+Aufnahme wird vorbereitet.
 """,
 
+    # OK
     TransitionEvent.STARTING_RECORDING_LEAVE: """
 Aufnahme gestartet.
 """,
 
+    # OK
     TransitionEvent.DELETING_LAST_TAKE_ENTER: """
 Versuche, die letzte Aufnahme zu löschen.
 """,
 
+    # OK
     TransitionEvent.DELETING_LAST_TAKE_LEAVE: """
 Löschen der letzten Aufnahme erfolgreich.
 """,
 
     # Menu: System.
 
+    # OK
     TransitionEvent.MOUNTING_STORAGE_ENTER: """
-Versuche das Speichermedium zu installieren.
+Versuche das Speichergerät zu aktivieren.
 """,
 
+    # OK
     TransitionEvent.MOUNTING_STORAGE_LEAVE: """
-Speichermedium erfolgreich installiert.
+Speichergerät erfolgreich aktiviert.
 """,
 
+    # OK
     TransitionEvent.DISCONNECTING_STORAGE_ENTER: """
-Versuche das Speichermedium zu trennen.
+Versuche das Speichergerät zu trennen.
 """,
 
+    # OK
     TransitionEvent.DISCONNECTING_STORAGE_LEAVE: """
-Speichermedium erfolgreich getrennt.
+Speichergerät erfolgreich getrennt.
 """,
 
+    # OK
     TransitionEvent.FORMATTING_STORAGE_ENTER: """
-Versuche das Speichermedium zu formatieren.
-Alle Daten auf diesem Speichermedium werden gelöscht.
+Versuche das Speichergerät zu formatieren.
+Alle Daten auf diesem Speichergerät werden gelöscht.
 """,
 
+    # OK
     TransitionEvent.FORMATTING_STORAGE_LEAVE: """
-Formatieren des Speichermediums erfolgreich.
+Formatieren des Speichergeräts erfolgreich.
 """,
 
+    # OK
     TransitionEvent.STOPPING_SYSTEM_ENTER: """
 Das System wird heruntergefahren.
 """,
 
     # Menu: OnRecord.
 
+    # OK
     TransitionEvent.HELPING_ONRECORD_LEAVE: """
 Das Menü „Aufnahme“.
 Drücken Sie „1“, um die Aufnahme zu stoppen.
@@ -295,11 +327,13 @@ Drücken Sie „2“, um einen Cue-Marker zu setzen.
 Drücken Sie „Stern“, um diese Nachricht zu wiederholen.
 """,
 
+    # OK
     TransitionEvent.STOPPING_RECORDING_ENTER: """
-Stoppe Aufnahme.
+Stoppe die Aufnahme.
 Dies kann einige Sekunden dauern.
 """,
 
+    # OK
     TransitionEvent.STOPPING_RECORDING_LEAVE: """
 Aufnahme gestoppt.
 Sie können jetzt zum Wiedergabemenü gehen und die Aufnahme anhören oder löschen.
@@ -320,39 +354,61 @@ Sie können jetzt zum Wiedergabemenü gehen und die Aufnahme anhören oder lösc
     TransitionEvent.PROCESSING_DIGIT_LEAVE: """""",
 
     # No audio menu necessary.
-    TransitionEvent.PROCESSING_DIGIT0_LEAVE: """""",
+    # OK
+    TransitionEvent.PROCESSING_DIGIT0_LEAVE: """
+Null""",
 
     # No audio menu necessary.
-    TransitionEvent.PROCESSING_DIGIT1_LEAVE: """""",
+    # OK
+    TransitionEvent.PROCESSING_DIGIT1_LEAVE: """
+Eins""",
 
     # No audio menu necessary.
-    TransitionEvent.PROCESSING_DIGIT2_LEAVE: """""",
+    # OK
+    TransitionEvent.PROCESSING_DIGIT2_LEAVE: """
+Zwei""",
 
     # No audio menu necessary.
-    TransitionEvent.PROCESSING_DIGIT3_LEAVE: """""",
+    # OK
+    TransitionEvent.PROCESSING_DIGIT3_LEAVE: """
+Drei""",
 
     # No audio menu necessary.
-    TransitionEvent.PROCESSING_DIGIT4_LEAVE: """""",
+    # OK
+    TransitionEvent.PROCESSING_DIGIT4_LEAVE: """
+Vier""",
 
     # No audio menu necessary.
-    TransitionEvent.PROCESSING_DIGIT5_LEAVE: """""",
+    # OK
+    TransitionEvent.PROCESSING_DIGIT5_LEAVE: """
+Fünf""",
 
     # No audio menu necessary.
-    TransitionEvent.PROCESSING_DIGIT6_LEAVE: """""",
+    # OK
+    TransitionEvent.PROCESSING_DIGIT6_LEAVE: """
+Sechs""",
 
     # No audio menu necessary.
-    TransitionEvent.PROCESSING_DIGIT7_LEAVE: """""",
+    # OK
+    TransitionEvent.PROCESSING_DIGIT7_LEAVE: """
+Sieben""",
 
     # No audio menu necessary.
-    TransitionEvent.PROCESSING_DIGIT8_LEAVE: """""",
+    # OK
+    TransitionEvent.PROCESSING_DIGIT8_LEAVE: """
+Acht""",
 
     # No audio menu necessary.
-    TransitionEvent.PROCESSING_DIGIT9_LEAVE: """""",
+    # OK
+    TransitionEvent.PROCESSING_DIGIT9_LEAVE: """
+Neun""",
 
     # No audio menu necessary.
+    # OK
     TransitionEvent.PROCESSING_DIGIT_OK_LEAVE: """""",
 
     # No audio menu necessary.
+    # OK
     TransitionEvent.PROCESSING_DIGIT_BACKSPACE_LEAVE: """""",
 
     # Menu: Playback
