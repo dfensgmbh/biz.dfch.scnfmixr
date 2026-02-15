46e228b5df3a1f019d9bbe277d0a013841f456d7
Irene Hofstetter <irene.hofstetter@npower.ch>
2026-02-15 19:01:54 +0100

    Transition event DE *added translations for transitions in DE

--- prep/snd/transition_event_de.py
+++ prep/snd/transition_event_de.py
@@ -24,41 +24,46 @@ from biz.dfch.scnfmixr.core.transition_event import TransitionEvent
 TransitionEventDe: dict[TransitionEvent, str] = {
 
     # Menu: Detect HID HI1.
+    # OK
     TransitionEvent.DETECTING_DEVICE_HI1_ENTER: """
-guid:eb686a85-3827-4f71-86d6-c7c3ca70c65f
+Versuche, Eingabegerät EINS zu erkennen.
 """,
-
+    # OK
     TransitionEvent.DETECTING_DEVICE_HI1_LEAVE: """
-guid:521f357c-58ad-4b43-b9d8-bd983cfc8def
+<<<sound-intro.wav>>>
 """,
-
+    # OK
     TransitionEvent.SKIPPING_DEVICE_HI1_LEAVE: """
-Eingabegerät H I EINS übersprungen.
+Eingabegerät EINS übersprungen.
 """,
 
     # Menu: Detect HID HI2.
+    # OK
     TransitionEvent.DETECTING_DEVICE_HI2_ENTER: """
-guid:beb5c507-6a5f-4520-a5f3-20c056ed3d59
+Versuche, Eingabegerät ZWEI zu erkennen.
 """,
 
     # DFTODO: Duplicate intro sound from HI1.
-    TransitionEvent.DETECTING_DEVICE_HI2_LEAVE: """""",
+    # OK
+    TransitionEvent.DETECTING_DEVICE_HI2_LEAVE: """
+<<<sound-intro.wav>>>""",
 
+    # OK
     TransitionEvent.SKIPPING_DEVICE_HI2_LEAVE: """
-Eingabegerät H I ZWEI übersprungen.
+Eingabegerät ZWEI übersprungen.
 """,
 
     # Menu: Detect HID HI3.
 
     TransitionEvent.DETECTING_DEVICE_HI3_ENTER: """
-guid:b1f37144-ba69-49e6-a0cc-dd7e15769d30
+Versuche, Eingabegerät DREI zu erkennen.
 """,
 
     # DFTODO: Duplicate intro sound from HI1.
     TransitionEvent.DETECTING_DEVICE_HI3_LEAVE: """""",
 
     TransitionEvent.SKIPPING_DEVICE_HI3_LEAVE: """
-Eingabegerät H I DREI übersprungen.
+Eingabegerät DREI übersprungen.
 """,
 
     # Menu: Detect Audio LCL.
@@ -78,93 +83,101 @@ guid:58411c9d-5876-4242-9862-9e10e3ad4846
     # Menu: Detect Audio EX1.
 
     TransitionEvent.DETECTING_DEVICE_EX1_ENTER: """
-guid:1508eb35-007c-4bb7-9f5a-40c6fdc96f9a
+Versuche, externes Gerät EX1 zu erkennen.
 """,
 
     TransitionEvent.DETECTING_DEVICE_EX1_LEAVE: """
-guid:a122c1e6-48de-46eb-b1fb-0805a02e77b0
+Externes Gerät EX1 erfolgreich erkannt.
 """,
 
     TransitionEvent.DETECTING_DEVICE_EX1_FAILED: """
-guid:ec999b2f-2c5e-490d-a689-e0a2ec5c0568
+Erkennung des externen Geräts EX1 fehlgeschlagen.
+
+Überprüfen Sie die Kabel und den Anschluss.
 """,
 
     TransitionEvent.SKIPPING_DEVICE_EX1_LEAVE: """
-guid:b812a5a6-8d11-4191-b801-908679354ba3
+Externes Gerät EX1 wurde übersprungen.
 """,
 
     # Menu: Detect Audio EX2.
 
     TransitionEvent.DETECTING_DEVICE_EX2_ENTER: """
-guid:46aa3aef-34ad-4506-a9cc-a358f33d61f3
+Versuche, externes Gerät EX2 zu erkennen.
 """,
 
     TransitionEvent.DETECTING_DEVICE_EX2_LEAVE: """
-guid:0cd3c8ba-720b-499f-9428-d38852345134
+Externes Gerät EX2 erfolgreich erkannt.
 """,
 
     TransitionEvent.DETECTING_DEVICE_EX2_FAILED: """
-guid:16a79ca7-016b-42a8-84b0-3e411c776698
+Erkennung des externen Geräts EX2 fehlgeschlagen.
+
+Überprüfen Sie die Kabel und den Anschluss.
 """,
 
     TransitionEvent.SKIPPING_DEVICE_EX2_LEAVE: """
-guid:f872de98-5d10-477a-af5c-7e36b2f9a18a
+Externes Gerät EX2 wurde übersprungen.
 """,
 
     # Menu: Detect storage RC1.
 
     TransitionEvent.DETECTING_DEVICE_RC1_ENTER: """
-guid:c4f3d141-6ba8-4c89-8788-edb496f4f4a5
+Versuche, Speichergerät RC1 zu erkennen.
 """,
 
     TransitionEvent.DETECTING_DEVICE_RC1_LEAVE: """
-guid:c07e7f10-fec4-4f5a-be1f-4a6cbd4ede87
+Speichergerät RC1 erfolgreich erkannt.
 """,
 
     TransitionEvent.DETECTING_DEVICE_RC1_FAILED: """
-guid:b8a21383-b68c-4884-9ad8-31d2c5a33f60
+Erkennung des Speichergerät RC1 fehlgeschlagen.
+
+Überprüfen Sie die Kabel und den Anschluss.
 """,
 
     TransitionEvent.SKIPPING_DEVICE_RC1_LEAVE: """
-guid:b59e03ef-d96f-4da6-92fb-e31e949f6e8a
+Speichergerät RC1 wurde übersprungen.
 """,
 
     # Menu: Detect storage RC2.
 
     TransitionEvent.DETECTING_DEVICE_RC2_ENTER: """
-guid:a351ad59-2306-4a28-80b8-2f06dc1ed806
+Versuche, Speichergerät RC2 zu erkennen.
 """,
 
     TransitionEvent.DETECTING_DEVICE_RC2_LEAVE: """
-guid:7db0153b-968d-485c-ba27-a87fc7347f7d
+Speichergerät RC2 erfolgreich erkannt.
 """,
 
     TransitionEvent.DETECTING_DEVICE_RC2_FAILED: """
-guid:c5cd1291-cad2-4d3f-ac76-5b66cbd59a85
+Erkennung des Speichergerät RC2 fehlgeschlagen.
+
+Überprüfen Sie die Kabel und den Anschluss.
 """,
 
     TransitionEvent.SKIPPING_DEVICE_RC2_LEAVE: """
-guid:ddd3bc6e-f60c-46df-b9d1-81e45bc9761c
+Speichergerät RC2 wurde übersprungen.
 """,
 
     # Menu: Clean storage RC1.
 
     TransitionEvent.CLEANING_DEVICE_RC1_ENTER: """
-guid:0eb65e0e-acb8-493b-b391-67f1bf48cb93
+Versuche, Aufnahmen vom Speichergerät RC1 zu löschen.
 """,
 
     TransitionEvent.CLEANING_DEVICE_RC1_LEAVE: """
-guid:a848f0b6-3a81-489c-af98-068c25781015
+Löschen der Aufnahmen vom Speichergerät RC1 erfolgreich.
 """,
 
     # Menu: Clean storage RC2.
 
     TransitionEvent.CLEANING_DEVICE_RC2_ENTER: """
-guid:d2896326-bd8f-4755-a297-92ba458e9e66
+Versuche, Aufnahmen vom Speichergerät RC2 zu löschen.
 """,
 
     TransitionEvent.CLEANING_DEVICE_RC2_LEAVE: """
-guid:ce9e7a9a-e6b5-4906-8af2-fe6b8e04908c
+Löschen der Aufnahmen vom Speichergerät RC2 erfolgreich.
 """,
 
 
@@ -217,73 +230,79 @@ Effektgerät 2 wurde übersprungen.
     # Menu: Initialise audio.
 
     TransitionEvent.INITIALISING_AUDIO_ENTER: """
-guid:21f72f0d-46fb-4c3b-9aa6-bb73e846771e
+Audio wird initialisiert
 """,
 
     TransitionEvent.INITIALISING_AUDIO_LEAVE: """
-guid:19921372-bcfe-4dd0-92a7-f881da22b36c
+Audio ist initialisiert
 """,
 
     # Menu: Main.
 
     TransitionEvent.STARTING_RECORDING_ENTER: """
-guid:f63f98b0-b7f5-4aed-ae64-7d7c90443026
+Bereite Aufnahme vor.
 """,
 
     TransitionEvent.STARTING_RECORDING_LEAVE: """
-guid:2010bcb1-b347-40d3-b83e-fafed2b9fcb9
+Aufnahme gestartet.
 """,
 
     TransitionEvent.DELETING_LAST_TAKE_ENTER: """
-guid:798f31ee-867d-46db-86ef-363f9499c7aa
+Versuche, die letzte Aufnahme zu löschen.
 """,
 
     TransitionEvent.DELETING_LAST_TAKE_LEAVE: """
-guid:e269d4a9-0fff-4dd4-8a0f-27a933d34b82
+Löschen der letzten Aufnahme erfolgreich.
 """,
 
     # Menu: System.
 
     TransitionEvent.MOUNTING_STORAGE_ENTER: """
-guid:e3129704-3385-4f3d-959c-39f8f2a8ceb4
+Versuche das Speichermedium zu installieren.
 """,
 
     TransitionEvent.MOUNTING_STORAGE_LEAVE: """
-guid:dea10229-e9d7-4919-87cf-60c28703b44a
+Speichermedium erfolgreich installiert.
 """,
 
     TransitionEvent.DISCONNECTING_STORAGE_ENTER: """
-guid:dc266b53-96f9-4928-b4cd-9604b1ee6853
+Versuche das Speichermedium zu trennen.
 """,
 
     TransitionEvent.DISCONNECTING_STORAGE_LEAVE: """
-guid:1bc3de7f-2fea-42f7-8716-aa7bf9e27c5a
+Speichermedium erfolgreich getrennt.
 """,
 
     TransitionEvent.FORMATTING_STORAGE_ENTER: """
-guid:35fe59ce-12b0-434d-aaa8-30ec962323f7
+Versuche das Speichermedium zu formatieren.
+Alle Daten auf diesem Speichermedium werden gelöscht.
 """,
 
     TransitionEvent.FORMATTING_STORAGE_LEAVE: """
-guid:7746cdfa-9c89-4b50-86d1-78221f69d136
+Formatieren des Speichermediums erfolgreich.
 """,
 
     TransitionEvent.STOPPING_SYSTEM_ENTER: """
-guid:7bf38858-c1db-4dc6-97dd-5be690c07e41
+Das System wird heruntergefahren.
 """,
 
     # Menu: OnRecord.
 
     TransitionEvent.HELPING_ONRECORD_LEAVE: """
-guid:48b0b1e0-b05d-41dc-99af-20964612b1e8
+Das Menü „Aufnahme“.
+Drücken Sie „1“, um die Aufnahme zu stoppen.
+Drücken Sie „2“, um einen Cue-Marker zu setzen.
+Drücken Sie „Stern“, um diese Nachricht zu wiederholen.
 """,
 
     TransitionEvent.STOPPING_RECORDING_ENTER: """
-guid:b8ba29a3-b93c-4617-a350-c507baca5bfb
+Stoppe Aufnahme.
+Dies kann einige Sekunden dauern.
 """,
 
     TransitionEvent.STOPPING_RECORDING_LEAVE: """
-guid:c5db16b3-b523-40ce-980a-772ae65c99ec
+Aufnahme gestoppt.
+Sie können jetzt zum Wiedergabemenü gehen und die Aufnahme anhören oder löschen.
 """,
 
     # No audio menu necessary.
