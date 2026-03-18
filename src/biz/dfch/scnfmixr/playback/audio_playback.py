e8d2a4bdd017f65b2f843703bc270df3c13245b8
Ronald Rink <dfch@users.noreply.github.com>
2026-03-18 15:29:20 +0100

    AudioPlayback
    * add more logging
    * remove redundant code
    * format code

--- src/biz/dfch/scnfmixr/playback/audio_playback.py
+++ src/biz/dfch/scnfmixr/playback/audio_playback.py
@@ -219,21 +219,30 @@ class AudioPlayback(IAcquirable):
         self._client.acquire()
         self._client.set_repeat(True)
 
-        # Load audio files from only the first available storage device (RC1 or
-        # RC2).
-        _queued_items = self._client.load_playback_queue(
-            lambda e: e.lower().startswith(MountPoint.RC1.name.lower()) and
-            FileName.is_valid_filename(e.removeprefix(
-                MountPoint.RC1.name.lower()).strip(os.sep)))
-        assert isinstance(_queued_items, list)
+        # Load audio files from only the first available storage device
+        # RC1 or RC2.
+        def load_playback_queue(
+            client: MediaPlayerClient,
+            mount_point: str
+        ) -> list[str]:
+            log.debug("Enumerating items from '%s' ...", mount_point)
+            _queued_items = client.load_playback_queue(
+                lambda e: e.lower().startswith(mount_point) and
+                FileName.is_valid_filename(e.removeprefix(
+                    mount_point).strip(os.sep)))
+            assert isinstance(_queued_items, list)
+            log.debug("Enumerating items from '%s' OK. [%s]", mount_point,
+                      len(_queued_items))
+            return _queued_items
+
+        mount_point = MountPoint.RC1.name.lower()
+        _queued_items = load_playback_queue(self._client, mount_point)
 
         if 0 == len(_queued_items):
-            _queued_items = self._client.load_playback_queue(
-                lambda e: e.lower().startswith(MountPoint.RC2.name.lower()) and
-                FileName.is_valid_filename(e.removeprefix(
-                    MountPoint.RC2.lower()).strip(os.sep)))
-        assert isinstance(_queued_items, list)
+            mount_point = MountPoint.RC2.name.lower()
+            _queued_items = load_playback_queue(self._client, mount_point)
 
+        assert isinstance(_queued_items, list)
         log.debug("Currently queued items: [%s]", _queued_items)
 
         for item in _queued_items:
@@ -243,7 +252,6 @@ class AudioPlayback(IAcquirable):
             log.debug("Try to get cue points in file: '%s' ...", fullname)
 
             seek_points = self.get_seekpoints(fullname)
-
             self._queued_items[item] = seek_points
 
             log.debug("Item '%s': [%s]", item, self._queued_items[item])
