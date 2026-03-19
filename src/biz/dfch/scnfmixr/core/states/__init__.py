82802706e4c521b0141c01491d47452526063ac4
Ronald Rink <dfch@users.noreply.github.com>
2026-03-19 09:15:57 +0100

    change copyright notice
    * change copyright notice to show 2026 for all files that changed since 2026-01-01

--- src/biz/dfch/scnfmixr/core/states/__init__.py
+++ src/biz/dfch/scnfmixr/core/states/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 d-fens GmbH, http://d-fens.ch
+# Copyright (c) 2025 - 2026 d-fens GmbH, http://d-fens.ch
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
