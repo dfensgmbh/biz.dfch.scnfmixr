1f3c6a6dd8cb64fa85807bf2b5609a8b5bfc5c2a
Ronald Rink <dfch@users.noreply.github.com>
2026-03-20 09:03:39 +0100

    Change Copyright
    * update test to create list of python files dynamically
    * remove static list of python files
    * ignore SonarCloud warning NOSONAR(pythonsecurity:S2083

--- tests/test_copyright_year.py
+++ tests/test_copyright_year.py
@@ -13,159 +13,28 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-"""Module test copyright."""
+# pylint: disable=C0114
+# pylint: disable=C0115
+# pylint: disable=C0116
+# pylint: disable=C0301
+
+"""Module update copyright."""
 
 from pathlib import Path
+import os
 import re
+import subprocess
 import unittest
 
 
+@unittest.skipIf(
+    os.getenv('GITHUB_ACTIONS') == 'true',
+    "This 'test' does change source files. Do only start it locally.")
 class TestCopyright(unittest.TestCase):
-    """Class testing template."""
-
-    # Get all py files that changed since 2026-01-01
-    # git log --since="2026-01-01" --name-only --pretty=format: | sls \.py | sort -u
-    files = """prep/snd/state_event_de.py
-prep/snd/state_event_en.py
-prep/snd/state_event_fr.py
-prep/snd/state_event_it.py
-prep/snd/transition_event_de.py
-prep/snd/transition_event_en.py
-prep/snd/transition_event_fr.py
-prep/snd/transition_event_it.py
-setup.py
-src/_text/__init__.py
-src/_text/MultiLineTextParser.py
-src/_text/MultiLineTextParserContext.py
-src/_text/TextUtils.py
-src/biz/__main__.py
-src/biz/dfch/scnfmixr/alsa_usb/alsa_stream_info_parser.py
-src/biz/dfch/scnfmixr/alsa_usb/alsa_stream_info_visitor.py
-src/biz/dfch/scnfmixr/app.py
-src/biz/dfch/scnfmixr/application_context.py
-src/biz/dfch/scnfmixr/args.py
-src/biz/dfch/scnfmixr/audio/audio_device_info.py
-src/biz/dfch/scnfmixr/core/fsm/execution_context.py
-src/biz/dfch/scnfmixr/core/state_event.py
-src/biz/dfch/scnfmixr/core/state_machine.py
-src/biz/dfch/scnfmixr/core/states/__init__.py
-src/biz/dfch/scnfmixr/core/states/change_language.py
-src/biz/dfch/scnfmixr/core/states/final_state.py
-src/biz/dfch/scnfmixr/core/states/initialise_audio.py
-src/biz/dfch/scnfmixr/core/states/initialise_ex1.py
-src/biz/dfch/scnfmixr/core/states/initialise_ex2.py
-src/biz/dfch/scnfmixr/core/states/initialise_hi1.py
-src/biz/dfch/scnfmixr/core/states/initialise_hi2.py
-src/biz/dfch/scnfmixr/core/states/initialise_hi3.py
-src/biz/dfch/scnfmixr/core/states/initialise_in1.py
-src/biz/dfch/scnfmixr/core/states/initialise_in2.py
-src/biz/dfch/scnfmixr/core/states/initialise_rc1.py
-src/biz/dfch/scnfmixr/core/states/initialise_rc2.py
-src/biz/dfch/scnfmixr/core/states/main.py
-src/biz/dfch/scnfmixr/core/states/onrecord.py
-src/biz/dfch/scnfmixr/core/states/playback.py
-src/biz/dfch/scnfmixr/core/states/system.py
-src/biz/dfch/scnfmixr/core/transition_event.py
-src/biz/dfch/scnfmixr/core/transitions/__init__.py
-src/biz/dfch/scnfmixr/core/transitions/cleaning_rc1.py
-src/biz/dfch/scnfmixr/core/transitions/cleaning_rc2.py
-src/biz/dfch/scnfmixr/core/transitions/clear_date_time_name.py
-src/biz/dfch/scnfmixr/core/transitions/deleting_last_take.py
-src/biz/dfch/scnfmixr/core/transitions/detecting_ex1.py
-src/biz/dfch/scnfmixr/core/transitions/detecting_ex2.py
-src/biz/dfch/scnfmixr/core/transitions/detecting_hi1.py
-src/biz/dfch/scnfmixr/core/transitions/detecting_hi2.py
-src/biz/dfch/scnfmixr/core/transitions/detecting_in1.py
-src/biz/dfch/scnfmixr/core/transitions/detecting_in2.py
-src/biz/dfch/scnfmixr/core/transitions/disconnecting_storage.py
-src/biz/dfch/scnfmixr/core/transitions/formatting_storage.py
-src/biz/dfch/scnfmixr/core/transitions/mounting_rc1.py
-src/biz/dfch/scnfmixr/core/transitions/mounting_rc2.py
-src/biz/dfch/scnfmixr/core/transitions/selecting_language_base.py
-src/biz/dfch/scnfmixr/core/transitions/showing_status.py
-src/biz/dfch/scnfmixr/core/transitions/skipping_ex1.py
-src/biz/dfch/scnfmixr/core/transitions/skipping_ex2.py
-src/biz/dfch/scnfmixr/core/transitions/skipping_in1.py
-src/biz/dfch/scnfmixr/core/transitions/skipping_in2.py
-src/biz/dfch/scnfmixr/core/transitions/skipping_lcl.py
-src/biz/dfch/scnfmixr/core/transitions/starting_recording.py
-src/biz/dfch/scnfmixr/core/transitions/starting_recording_mixes.py
-src/biz/dfch/scnfmixr/core/transitions/toggling_mute.py
-src/biz/dfch/scnfmixr/core/transitions/unmounting_rc1.py
-src/biz/dfch/scnfmixr/core/transitions/unmounting_rc2.py
-src/biz/dfch/scnfmixr/date_time_name_input.py
-src/biz/dfch/scnfmixr/devices/storage/detecting_rc_worker.py
-src/biz/dfch/scnfmixr/devices/storage/device_operations.py
-src/biz/dfch/scnfmixr/input/streamdeck_image_converter.py
-src/biz/dfch/scnfmixr/input/streamdeck_image_library.py
-src/biz/dfch/scnfmixr/input/streamdeck_input_resolver.py
-src/biz/dfch/scnfmixr/jack_commands/alsa_jack_base.py
-src/biz/dfch/scnfmixr/jack_commands/zita_bridge_base.py
-src/biz/dfch/scnfmixr/mixer/audio_recorder.py
-src/biz/dfch/scnfmixr/mixer/device_factory.py
-src/biz/dfch/scnfmixr/mixer/jack_alsa_device.py
-src/biz/dfch/scnfmixr/mixer/jack_signal_manager.py
-src/biz/dfch/scnfmixr/mixer/jack_signal_point_manager.py
-src/biz/dfch/scnfmixr/mixer/signal_point.py
-src/biz/dfch/scnfmixr/playback/audio_menu.py
-src/biz/dfch/scnfmixr/playback/audio_playback.py
-src/biz/dfch/scnfmixr/playback/media_player_client.py
-src/biz/dfch/scnfmixr/playback/media_player_type.py
-src/biz/dfch/scnfmixr/public/__init__.py
-src/biz/dfch/scnfmixr/public/audio/alsa_interface_info.py
-src/biz/dfch/scnfmixr/public/audio/audio_device.py
-src/biz/dfch/scnfmixr/public/audio/bit_depth.py
-src/biz/dfch/scnfmixr/public/audio/format.py
-src/biz/dfch/scnfmixr/public/constant.py
-src/biz/dfch/scnfmixr/public/input/__init__.py
-src/biz/dfch/scnfmixr/public/input/event_map_base.py
-src/biz/dfch/scnfmixr/public/input/keyboard_event_map.py
-src/biz/dfch/scnfmixr/public/input/menu_profile.py
-src/biz/dfch/scnfmixr/public/input/streamdeck_event_map.py
-src/biz/dfch/scnfmixr/public/messages/audio_recorder.py
-src/biz/dfch/scnfmixr/public/mixer/connection_info.py
-src/biz/dfch/scnfmixr/public/mixer/connection_policy.py
-src/biz/dfch/scnfmixr/public/storage/file_name.py
-src/biz/dfch/scnfmixr/public/system/__init__.py
-src/biz/dfch/scnfmixr/public/system/usb_port.py
-src/biz/dfch/scnfmixr/public/ui/ui_parameters.py
-src/biz/dfch/scnfmixr/system/action_descriptor.py
-src/biz/dfch/scnfmixr/system/message_queue.py
-src/biz/dfch/scnfmixr/system/signal_handler.py
-src/biz/dfch/scnfmixr/system/timer.py
-src/biz/dfch/scnfmixr/ui/event_handler_base.py
-src/biz/dfch/scnfmixr/ui/keyboard_handler.py
-src/biz/dfch/scnfmixr/ui/streamdeck_handler.py
-src/text/__init__.py
-src/text/MultiLineTextParser.py
-src/text/MultiLineTextParserContext.py
-src/text/TextUtils.py
-tests/_text/__init__.py
-tests/_text/test_MultiLineTextParser.py
-tests/_text/test_TextUtils.py
-tests/alsa_usb/test_AlsaStreamInfoVisitor.py
-tests/scnfmixr/core/fsm/test_transition_base.py
-tests/scnfmixr/input/test_streamdeck_image_converter.py
-tests/scnfmixr/input/test_streamdeck_image_library.py
-tests/scnfmixr/input/test_streamdeck_input_resolver.py
-tests/scnfmixr/mixer/test_signal_point.py
-tests/scnfmixr/public/audio/__init__.py
-tests/scnfmixr/public/audio/test_format.py
-tests/scnfmixr/public/input/__init__.py
-tests/scnfmixr/public/input/test_menu_profile.py
-tests/scnfmixr/res/test_res_snd_files.py
-tests/scnfmixr/res/test_state_de.py
-tests/scnfmixr/res/test_transition_de.py
-tests/scnfmixr/res/test_transition_en.py
-tests/scnfmixr/res/test_transition_fr.py
-tests/scnfmixr/res/test_transition_it.py
-tests/scnfmixr/test_date_time_name_input.py
-tests/test___main__.py
-tests/text/__init__.py
-tests/text/test_MultiLineTextParser.py
-tests/text/test_TextUtils.py"""
+    """Change copyright year to include specified year."""
 
     CURRENT_YEAR = "2026"  # or str(datetime.now().year)
+
     _pattern = re.compile(
         r"^(# Copyright \(c\) )(.+?)( d-fens GmbH, http://d-fens\.ch)$"
     )
@@ -191,24 +60,45 @@ tests/text/test_TextUtils.py"""
         assert path.exists(), path
         lines = path.read_text(encoding="utf-8").splitlines()
         if not lines:
-            return None
+            return
         updated = self._update_line(lines[0])
         if not updated:
-            return None
+            return
         lines[0] = updated
-        path.write_text("\n".join(lines) + "\n", encoding="utf-8")
+        path.write_text(
+            "\n".join(lines) + "\n",
+            encoding="utf-8"
+        )  # NOSONAR(pythonsecurity:S2083)
 
-    def test_something(self):
-        """Testing something succeeds."""
+    def test_update_copyright_year(self):
 
         project_dir = Path(__file__).resolve().parent.parent
-        for file in self.files.split(sep="\n"):
+
+        # git log --since="2026-01-01" --name-only --pretty=format:
+        cmd = [
+            "git.exe",
+            "log",
+            f'--since="{self.CURRENT_YEAR}-01-01"',
+            "--name-only",
+            "--pretty=format:",
+        ]
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+        files = sorted({
+            e.strip() for e
+            in result.stdout.splitlines()
+            if e.strip().lower().endswith(".py")
+        })
+        for file in files:
             full_name = project_dir / file
             if not Path.exists(full_name):
                 continue
             print(f"{full_name}")
             self._process_file(full_name)
-
 
 if __name__ == "__main__":
     unittest.main()
